9764af1c96928121413ed0be4b0a36a03f639c9c
pierreadorni <pierreadorni@free.fr>
2023-02-15 14:53:56 +0100

    add ACME automation policy parameters

--- automation/automation.go
+++ automation/automation.go
@@ -1,9 +1,13 @@
 package automation
 
 type Policy struct {
-	KeyType           string              `json:"keyType"`
-	Module            string              `json:"module"`
-	Profile           string              `json:"profile"`
-	TrustChain        map[string][]string `json:"trustChain"`
-	AuthorizationMode string              `json:"authorizationMode,omitempty"`
+	KeyType    string              `json:"keyType"`
+	Module     string              `json:"module"`
+	Profile    string              `json:"profile"`
+	TrustChain map[string][]string `json:"trustChain"`
+	// EST only parameters
+	AuthorizationMode string `json:"authorizationMode,omitempty"`
+	// ACME only parameters
+	TlsAlpn01Port int `json:"tlsAlpn01Port,omitempty"`
+	Http01Port    int `json:"http01Port,omitempty"`
 }
